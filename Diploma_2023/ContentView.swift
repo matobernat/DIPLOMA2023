5c45bf6478eefe1569155e0cdf3dd966e3659fb9
Martin Bernát <bernam11@fel.cvut.cz>
2023-02-25 09:44:46 +0100

    Initial Commit

---
+++ Diploma_2023/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Diploma_2023
+//
+//  Created by Martin Bernát on 25/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Diploma_2023/Diploma_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Diploma_2023App.swift
+//  Diploma_2023
+//
+//  Created by Martin Bernát on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Diploma_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
